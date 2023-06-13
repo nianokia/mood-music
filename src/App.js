449ddb2744bbb0c7c840bcc30b86707e0f35705c
nianokia <111546789+nianokia@users.noreply.github.com>
2023-06-13 19:19:15 -0400

    Added Form, Search functions, & Music component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Music from "./Music";
 
 import "./App.css";
 
 function App() {
+  let [query, setQuery] = useState("");
+  let [color, setColor] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function updateQuery(event) {
+    setQuery(event.target.value);
+  }
+
+  function updateColor(event) {
+    setColor(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Mood Music</h1>
-      <form>
-        <input type="color" />
-        <input type="search" />
-        <input type="submit" value="Search" />
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label for="color">Set Color:</label>
+          <input
+            type="color"
+            id="color"
+            className="color"
+            onChange={updateColor}
+          />
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <input
+              type="search"
+              className="col-9 form-control w-100"
+              placeholder="Enter a mood..."
+              autoFocus="on"
+              onChange={updateQuery}
+            />
+          </div>
+          <div className="col-4">
+            <input
+              type="submit"
+              value="Search"
+              className="col-3 form-control w-100"
+            />
+          </div>
+        </div>
       </form>
+      <Music query={query} color={color} />
     </div>
   );
 }
---
+++ src/Music.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Music(props) {
+  return (
+    <div className="Music">
+      <header>Hello from Music</header>
+    </div>
+  );
+}
