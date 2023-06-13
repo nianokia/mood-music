8ab157d675f3a6f90364fbc84478547af3dd0af6
nianokia <111546789+nianokia@users.noreply.github.com>
2023-06-13 19:43:23 -0400

    Showed form results through Music Comp

--- src/Music.js
+++ src/Music.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Music(props) {
   return (
-    <div className="Music">
-      <header>Hello from Music</header>
+    <div className="Music mt-4">
+      <div>You searched for {props.query}</div>
+      <div>You chose this color {props.color}</div>
     </div>
   );
 }
